13debcfeb6de844ddb04ca2fa6b0bbb5a0260f8a
Stefan Oswald <Stefanpauloswald@gmail.com>
2015-03-30 13:57:13 -0400

    Update findAndMoveCoverageFile.py

--- findAndMoveCoverageFile.py
+++ findAndMoveCoverageFile.py
@@ -1,11 +1,10 @@
 import os
-import shutil
-
 fp = open("C:/Program Files (x86)/Jenkins/jobs/Baby Professor/workspace/CodeCoverage/tmp/vsoutput.txt")
 coverage = ".coverage"
 for line in fp:
  if line.find(coverage) > -1:
-  shutil.move(line.strip(), "C:/Program Files (x86)/Jenkins/jobs/Baby Professor/workspace/CodeCoverage/tmp/raw.coverage")
-  os.rename(line.strip(), "C:/Program Files (x86)/Jenkins/jobs/Baby Professor/workspace/CodeCoverage/tmp/raw.coverage")
+  source = line.strip()
+  print source
+  os.rename(source, "C:/Program Files (x86)/Jenkins/jobs/Baby Professor/workspace/CodeCoverage/tmp/raw.coverage")
 
 fp.close()
