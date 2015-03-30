1afd792335f8d119738c7715257ed081134829e5
Stefan Oswald <Stefanpauloswald@gmail.com>
2015-03-30 10:58:45 -0400

    Update findAndMoveCoverageFile.py

--- findAndMoveCoverageFile.py
+++ findAndMoveCoverageFile.py
@@ -4,6 +4,7 @@ fp = open("C:/Program Files (x86)/Jenkins/jobs/Baby Professor/workspace/CodeCove
 coverage = ".coverage"
 for line in fp:
  if line.find(coverage) > -1:
+  print line.strip()
   os.rename(line.strip(), "C:/Program Files (x86)/Jenkins/jobs/Baby Professor/workspace/CodeCoverage/tmp/raw.coverage")
 
 
