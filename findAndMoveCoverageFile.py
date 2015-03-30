6c2c3436a974e2801657e4bdf70afed0499d2d00
Stefan Oswald <Stefanpauloswald@gmail.com>
2015-03-29 22:22:05 -0400

    Update findAndMoveCoverageFile.py
    vghkfjklj

--- findAndMoveCoverageFile.py
+++ findAndMoveCoverageFile.py
@@ -1,6 +1,6 @@
 import os
 
-fp = open("tmp/vsoutput.txt")
+fp = open("C:/Program Files (x86)/Jenkins/jobs/Baby Professor/workspace/CodeCoverage/tmp/vsoutput.txt")
 coverage = ".coverage"
 for line in fp:
  if line.find(coverage) > -1:
