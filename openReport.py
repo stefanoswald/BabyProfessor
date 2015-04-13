27956e88adc2aed6c45e185a804a2294ad9a205b
soswald3647 <soswald3647@eagle.fgcu.edu>
2015-04-13 11:52:22 -0400

    Python and GMTests

---
+++ openReport.py
@@ -0,0 +1,5 @@
+import webbrowser
+import os
+# webbrowser.get("Windows-default").open("file://C:/BabyProfessor/BabyProfessor/GMTests.html", new=1, autoraise=True)
+# webbrowser.get('mozilla').open("Google.com", new=1, autoraise=True)
+os.startfile("file://C:/BabyProfessor/BabyProfessor/GMTests.html/")
