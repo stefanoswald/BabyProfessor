e549b45e10081a1484dfc826dc270f9d2f7075a0
Stefan Oswald <Stefanpauloswald@gmail.com>
2015-04-13 11:56:38 -0400

    Update OpenCoverage.bat

--- OpenCoverage.bat
+++ OpenCoverage.bat
@@ -1,3 +1,4 @@
 start chrome "C:\BabyProfessor\BabyProfessor\GMTests.html"
 echo "try to open google"
 start chrome https://www.google.com/
+start "C:\Program Files (x86)\Python34\python.exe" C:\BabyProfessor\BabyProfessor\openReport.py
